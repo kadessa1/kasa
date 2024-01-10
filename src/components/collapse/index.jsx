cd237244ecc33aea76d44e006ae04852b532276b
kadessa1 <kadessa.dss@gmail.com>
2024-01-10 21:44:45 +0100

    ajout des bullets point + animation

--- src/components/collapse/index.jsx
+++ src/components/collapse/index.jsx
@@ -5,8 +5,11 @@ import arrow from '../../assets/pictures/arrow.png'
 function Collapse({collapseTitle , collapseDescription}){
     const [isOpen, setIsOpen] = useState(false)
 
+
+
     const handleClick = ()=>{
         setIsOpen(!isOpen)
+
     }
 
     return(
@@ -16,11 +19,14 @@ function Collapse({collapseTitle , collapseDescription}){
                 <img  src = {arrow} className={isOpen ?`${styles.arrow_up}`:`${styles.arrow_down}`}
                 alt='deployer' />
             </div>
-            {isOpen && (
-                <div className={styles.collapse_description}>
-                    <div>{collapseDescription}</div>
+            
+                <div className={styles.collapse_description + " " + (isOpen ?  styles.open: "")}  >
+                    <div >
+                        <div className={styles.inner}>    
+                            {collapseDescription}
+                        </div>
+                    </div>
                 </div>
-            )}
         </div>
     )
 }
--- src/components/slideshow/index.jsx
+++ src/components/slideshow/index.jsx
@@ -5,12 +5,49 @@ import styles from './style.module.css';
 
 function Slideshow({pictures, picturesLength}) {
     const [index, setIndex] = useState(0);
+
+
+    const initiateSlideState = ()=> {
+        document.querySelectorAll(`.${styles.carrousel_container } img `).forEach(e=> {
+            e.style.transform = null
+            e.style.zIndex = null
+        })
+    }
+
+
     const prevImg = (index) => {
+
+        initiateSlideState()
+         if(index == 0){     
+            let curr = document.querySelectorAll(`.${styles.carrousel_container} img `)[picturesLength -index -1]
+            curr.style.transform = `translateX(${-(picturesLength - index-1) *100}%)`
+         }else{
+            let curr = document.querySelectorAll(`.${styles.carrousel_container} img `)[index-1]
+            curr.style.transform = `translateX(${-(index -1) *100}%)`
+            curr.style.zIndex = 100 - index
+         }
         index > 0 ? setIndex(index - 1) : setIndex((index = pictures.length - 1))
     };
+
+
+
     const nextImg = (index) => {
+        initiateSlideState()
+        if(index + 1 >=  picturesLength){
+            document.querySelectorAll(`.${styles.carrousel_container } img `).forEach(e=> {
+                e.style.transform = null
+            })
+            
+        }else{
+            let next = document.querySelectorAll(`.${styles.carrousel_container } img `)[index+1] ; 
+            next.style.transform = `translateX(${-(index+1) *100}%)`
+        }
         index < picturesLength - 1 ? setIndex(index + 1) : setIndex((index = 0))
-    };
+
+     };
+
+
+
     if (picturesLength === 1) {
         return (
             <div className={styles.carrousel_container}>
@@ -24,13 +61,15 @@ function Slideshow({pictures, picturesLength}) {
         );
     } else if (picturesLength > 0) {
         return (
+        <div style={{position: "relative"}}>
             <div className={styles.carrousel_container}>
-                <img
-                    className={styles.slide}
-                    src={pictures[index]}
+                {pictures.map((p,i)=> <img
+                    className={styles.slide + " " + ( i == 0 ? styles.visible : "") }
+                    src={p} 
                     alt="Visuel de l'appartement"
-                    key={`Slideshow-${index}`}
-                />
+                    key={`Slideshow-${i}`}
+                />)}
+               
                 <div className={styles.arrow_container}>
                     <button className={styles.prev} onClick={() => prevImg(index)}>
                         <img
@@ -45,10 +84,11 @@ function Slideshow({pictures, picturesLength}) {
                         />
                     </button>
                 </div>
-                <p className={styles.number}>
-                    {index + 1} / {picturesLength}
-                </p>
             </div>
+               <div className={styles.bullets}>
+               {pictures.map((p,i)=> (<input type='radio' name={`radio-${i}`}  key={i}  { ... i== index ? {checked: true} : {checked: false}} ></input>))}
+           </div>
+        </div>
         )
     }
 };
